657bf7f581dd2fdafea8f9c986180eeb1a91a7e9
Henry Wu <2316687+henry40408@users.noreply.github.com>
2020-03-16 14:47:19 +0800

    feat(hyper): update font

--- .hyper.js
+++ .hyper.js
@@ -13,7 +13,7 @@ module.exports = {
 
     // font family with optional fallbacks
     fontFamily:
-      '"FuraCode Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      '"Fira Code Retina Nerd Font Complete", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: "normal",
