91c5559270659c7c12f21b95a577f68b8d4047d7
Heng-Yi <2316687+henry40408@users.noreply.github.com>
2020-05-05 16:16:11 +0800

    refactor: remove unused script

--- .zsh/setup-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/bin/env git --git-dir=${HOME}/.cfg --work-tree=${HOME} \
-    config --local status.showUntrackedFiles no
-
-/usr/bin/env git config --global pager.diff "diff-so-fancy | less --tabs=1,5 -RFX"
-/usr/bin/env git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
