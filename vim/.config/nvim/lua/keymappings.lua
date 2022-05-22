6fe0f2b720402381a74ddd32bb1d0f790c9b229f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-05-22 22:15:18 +0800

    refactor(vim): remove unused keymappings

--- vim/.config/nvim/lua/keymappings.lua
+++ vim/.config/nvim/lua/keymappings.lua
@@ -14,11 +14,6 @@ map_key('v', '<', '<gv', { noremap = true })
 -- My mappings
 map_key('n', '<Leader>c', '<cmd>close<CR>', { noremap = true, silent = true })
 map_key('n', '<Leader>q', '<cmd>q<CR>', { noremap = true, silent = true })
-map_key('n', '<Leader>r', '<cmd>zR<CR>', { noremap = true, silent = true })
 map_key('n', '<Leader>ve', '<cmd>e $MYVIMRC<CR>', { noremap = true, silent = true })
 local c = sf('<cmd>so $MYVIMRC<CR><cmd>PackerCompile<CR><cmd>echo "sourced %s"<CR>', os.getenv('MYVIMRC'))
 map_key('n', '<Leader>vs', c, { noremap = true, silent = true })
-
--- map <Esc> to exit terminal-mode
--- ref: https://neovim.io/doc/user/nvim_terminal_emulator.html
-map_key('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
