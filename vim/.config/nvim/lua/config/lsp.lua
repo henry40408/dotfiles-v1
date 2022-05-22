9d441389e7257f69cbc8d1d8abf28b7a2a0b97a8
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-05-22 23:06:13 +0800

    refactor(vim): customize telescope

--- vim/.config/nvim/lua/config/lsp.lua
+++ vim/.config/nvim/lua/config/lsp.lua
@@ -27,12 +27,13 @@ lsp.tsserver.setup({
 })
 
 --ref: https://sharksforarms.dev/posts/neovim-rust/
-map_key('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-map_key('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
-map_key('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
-map_key('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
-map_key('n', '1gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
-map_key('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-map_key('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
-map_key('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
-map_key('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+map_key('n', '<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map_key('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+map_key('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+map_key('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+map_key('n', '1gD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+map_key('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map_key('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+map_key('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
+map_key('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
--- vim/.config/nvim/lua/config/telescope.lua
+++ vim/.config/nvim/lua/config/telescope.lua
@@ -1,13 +1,25 @@
 local map_key = vim.keymap.set
-require('telescope').setup()
-map_key('n', '<Leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', { noremap = true, silent = true })
-map_key('n', '<Leader>fc', '<cmd>lua require("telescope.builtin").commands()<CR>', { noremap = true, silent = true })
-map_key('n', '<Leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>', { noremap = true, silent = true })
-map_key('n', '<Leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', { noremap = true, silent = true })
-map_key('n', '<Leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>', { noremap = true, silent = true })
-map_key(
-  'n',
-  '<Leader>fs',
-  '<cmd>lua require("telescope.builtin").colorscheme({ enable_preview = true })<CR>',
-  { noremap = true, silent = true }
-)
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      horizontal = {
+        preview_width = 0.6,
+        width = 0.99,
+      },
+    },
+  },
+  pickers = {
+    buffers = {
+      theme = 'dropdown',
+      previewer = false,
+    },
+  },
+})
+
+local opts = { noremap = true, silent = true }
+map_key('n', '<Leader>fb', '<cmd>Telescope buffers<CR>', opts)
+map_key('n', '<Leader>fc', '<cmd>Telescope commands<CR>', opts)
+map_key('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', opts)
+map_key('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+map_key('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>', opts)
+map_key('n', '<Leader>fs', '<cmd>Telescope colorscheme enable_preview=true<CR>', opts)
