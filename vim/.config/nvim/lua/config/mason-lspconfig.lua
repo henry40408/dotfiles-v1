dcb8584c93a73ee49b42fadcd3069d9f47c7c12f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-11-11 23:33:01 +0800

    refactor: add language servers

--- vim/.config/nvim/lua/config/mason-lspconfig.lua
+++ vim/.config/nvim/lua/config/mason-lspconfig.lua
@@ -1,3 +1,13 @@
 require('mason-lspconfig').setup({
-  ensure_installed = { 'sumneko_lua', 'rust_analyzer' }
+  ensure_installed = {
+    'eslint',
+    'jsonls',
+    'pyright',
+    'rust_analyzer',
+    'sumneko_lua',
+    'taplo',
+    'tflint',
+    'tsserver',
+    'yamlls',
+  }
 })
